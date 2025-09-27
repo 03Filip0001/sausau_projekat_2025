68ec916b8662ac7e7919cc465e504d613b0e64d3
03Filip0001 <hackboyiv@gmail.com>
2025-09-27 12:52:10 +0200

    Using split_data()

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ print("Projekat IV: Telco Customer Churn")
 print("######################################\n\n")
 
 from src.load_data import load_data, parse_data
+from src.split_data import split_data
 
 # Loading data
 data = load_data(fName="data/telco_data.csv")
 
 # Preproccess data
 data, encoders = parse_data(data=data)
+
+# Spliting data
+X_train, X_test, y_train, y_test = split_data(data=data, train_size=0.7)
 
 print(data.head())
