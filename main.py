a0e4bde50c22be04c8986800e3ab1c3e1d81fc1c
03Filip0001 <hackboyiv@gmail.com>
2025-09-27 11:31:29 +0200

    Merged with load_data and using parse_data()

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ print("\n\n######################################")
 print("Projekat IV: Telco Customer Churn")
 print("######################################\n\n")
 
-from src.load_data import load_data
+from src.load_data import load_data, parse_data
 
+# Loading data
 data = load_data(fName="data/telco_data.csv")
+
+# Preproccess data
+data, encoders = parse_data(data=data)
 
 print(data.head())
