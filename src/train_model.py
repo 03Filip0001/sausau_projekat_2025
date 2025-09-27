a71ef98605c152b0aac3c765662fa0c87667844c
03Filip0001 <hackboyiv@gmail.com>
2025-09-27 14:40:19 +0200

    Training X same models with different hyperparameters

--- src/train_model.py
+++ src/train_model.py
@@ -3,34 +3,30 @@ from src.utils import _print_msg
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
+_max_iter = [100, 250, 500, 1000, 2000]
+_n_estimators = [25, 50, 100, 150, 300]
+
 configuration_logistic = {
-	"max_iter": 1000,
+	"max_iter": _max_iter[0],
 	"solver": "lbfgs",
 	"class_weight": "balanced",
 	"random_state": 0
 }
 
 configuration_forest = {
-	"n_estimators": 100,
+	"n_estimators": _n_estimators[0],
 	"class_weight": "balanced",
 	"random_state": 0
 }
 
 configuration_gradient = {
-	"n_estimators": 100,
+	"n_estimators": _n_estimators[0],
 	"learning_rate": 0.1,
 	"random_state": 0
 }
 
-def train_model(X_train=None, y_train=None, model_class=None, model_name="", random_state=42):
+def _train_model(X_train=None, y_train=None, model_class=None, model_name="", random_state=42):
 	_print_msg(msg="Training model "+model_name)
-
-	if (X_train is None) or (y_train is None):
-		raise Exception("[train_model] Provide X_train and y_train values")
-
-	if model_class is None:
-		raise Exception("[train_model] Provide model")
-	
 	_print_msg(msg="With configuration: ")
 
 	if model_class == LogisticRegression:
@@ -57,5 +53,27 @@ def train_model(X_train=None, y_train=None, model_class=None, model_name="", ran
 	model = model.fit(X_train, y_train)
 
 	_print_msg(msg="Done training model "+model_name, nl=True, sep=True)
+
+	return model
+
+def train_model(X_train=None, y_train=None, model_class=None, model_name="", random_state=42):
+	if (X_train is None) or (y_train is None):
+		raise Exception("[train_model] Provide X_train and y_train values")
+
+	if model_class is None:
+		raise Exception("[train_model] Provide model")
+	
+	model = {}
+
+	if model_class == LogisticRegression:
+		for iter in _max_iter:
+			configuration_logistic["max_iter"] = iter
+			model[iter] = _train_model(X_train=X_train, y_train=y_train, model_class=model_class, model_name=model_name, random_state=random_state)
+
+	else:
+		for n in _n_estimators:
+			configuration_forest["n_estimators"] = n
+			configuration_gradient["n_estimators"] = n
+			model[n] = _train_model(X_train=X_train, y_train=y_train, model_class=model_class, model_name=model_name, random_state=random_state)
 
 	return model
