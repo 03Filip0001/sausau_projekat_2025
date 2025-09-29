68d13692d9b852ad7edfd663c8ab20cb5881a9af
03Filip0001 <hackboyiv@gmail.com>
2025-09-29 15:50:25 +0200

    Added comments to config.py

--- config.py
+++ config.py
@@ -1,19 +1,30 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
-DEFAULT_MODEL_CLASS = GradientBoostingClassifier
-DEFAILT_MODEL_NAME = "Gradient Boosting"
+"""
+	Default model which will be run when program starts
+	In order to run another model with best trained parameters uncomment below variables in group
+"""
+# DEFAULT_MODEL_CLASS = GradientBoostingClassifier
+# DEFAILT_MODEL_NAME = "Gradient Boosting"
 
 # DEFAULT_MODEL_CLASS = LogisticRegression
 # DEFAILT_MODEL_NAME = "Logistic Regression"
 
-# DEFAULT_MODEL_CLASS = RandomForestClassifier
-# DEFAILT_MODEL_NAME = "Forest Classifier"
+DEFAULT_MODEL_CLASS = RandomForestClassifier
+DEFAILT_MODEL_NAME = "Forest Classifier"
 
+
+"""
+	Default files location
+	DO NOT CHANGE!
+"""
 DEFAULT_DATA_PATH = "data/telco_data.csv"
 DEFAULT_PARAMS_PATH = "data/params.json"
 
-# Models used for this project
+""" 
+	Models used for this project
+"""
 MODEL_NAMES = {
 	"Logistic Regression": LogisticRegression,
 	"Forest Classifier": RandomForestClassifier,
